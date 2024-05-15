ee1b855f6612457d6843ceeb952190b6107c9829
Priya Tyagi <priyatyagi1905@gmail.com>
2024-05-14 20:03:46 -0700

    fixed moviecard.jsx

--- src/pages/MovieCard.jsx
+++ src/pages/MovieCard.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
-import Ratings from './Ratings';
 import '../styles/movieCard.css';
-import favicon from '../assets/fav-icon.svg'; 
+
 
 const MovieCard = ({ movie }) => {
   const [showOverview, setShowOverview] = useState(true);
+
 
   const handleMouseEnter = () => {
     setShowOverview(true);
@@ -15,29 +15,42 @@ const MovieCard = ({ movie }) => {
     setShowOverview(false);
   };
 
-  const getShortenedOverview = () => {
+
+   // Function to get the first 20 words of the overview
+   const getShortenedOverview = () => {
     const words = movie.overview.split(' ');
     return words.slice(0, 20).join(' ');
   };
+  
+ 
 
   return (
-    <section className="movie-card">
+    <section className="movie-card" 
+    
+    >
       <div>
         <div className='hover-overview'>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-          <img id='favorite-color' src={favicon} alt="fav-icon" />
-          {showOverview && <p className="overview">{getShortenedOverview()}...</p>}
+        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+        
+        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" id='favorite-color'><path d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg>
+         {showOverview && <p className="overview">{getShortenedOverview()}...</p>}
+         <button className="btn-hover">
+         <NavLink to={`/single/${movie.id}`}>
+            More Info
+            </NavLink>
+            </button>
         </div>
 
-        <h6>{movie.title}</h6>
-        <p>Release Date: {movie.release_date}</p>
-        <p>Rating: {movie.vote_average}</p>
-        <button className='more-info'>
-          <NavLink to={`/Single/${movie.id}`}>
-            More Info
-          </NavLink>
+  
+      <h6>{movie.title}</h6>
+      <p>Release Date: {movie.release_date}</p>
+      <p>Rating: {movie.vote_average}</p>
+      <button className='more-info'>
+      <NavLink to={`/single/${movie.id}`}>
+        More Info
+        </NavLink>
         </button>
-        <Ratings />
+      
       </div>
     </section>
   );
