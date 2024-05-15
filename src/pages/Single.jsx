7f655cb8b1304bef5fb1e5a655f18fd0c94d5be5
Simran <144748447+Simran1997161@users.noreply.github.com>
2024-05-14 19:57:06 -0700

    single updated

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -2,35 +2,53 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 const Single = () => {
-
   const [singleMovie, setSingleMovie] = useState();
   let {id} = useParams();
-
   useEffect(() => {
+    // Fetch movie data using the movie ID from an API
     const fetchMovieData = async () => {
       try {
         const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`);
-        const data = await response.json();
-        setMovie(data);
-      } catch (e) {
+        if (!response.ok) {
+          throw new Error('Failed to fetch movie data');
+        }
+        const movieData = await response.json();
+        setSingleMovie(movieData);
+      } catch (error) {
+        console.error(error);
       }
     };
     fetchMovieData();
-  }, [id]);
-
+  }, []);
   return (
     <div className='container'>
       <div>
-        <h2>{movie.title}</h2>
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-        <p>Release Date: {movie.release_date}</p>
-        <p>Overview: {movie.overview}</p>
-        <p>Rating: {movie.vote_average}</p>
+        {singleMovie && (
+          <div>
+             <img src={`https://image.tmdb.org/t/p/w500/${singleMovie.poster_path}`} alt={singleMovie.title} />
+             <h2>{singleMovie.title}</h2>
+             <p>Release Date: {singleMovie.release_date}</p>
+             <p>Overview: {singleMovie.overview}</p>
+             <p>Rating: {singleMovie.vote_average}</p>
+            </div>
+        )}
       </div>
     </div>
   );
 }
-
 export default Single;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
