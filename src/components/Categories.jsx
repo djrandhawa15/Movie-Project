1c6887745735bea3c84a2a79308463469abf593d
Priya Tyagi <priyatyagi1905@gmail.com>
2024-05-27 12:07:09 -0700

    remove _ from tabs and formatted them in uppercase

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,42 +1,37 @@
-import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import '../styles/categories.css';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "../styles/categories.css";
 
-const Categories= ({ fetchMovies }) =>  {
-
+const Categories = ({ fetchMovies }) => {
   // array of categories
-    const listofMovies = [ "popular", "top_rated", "now_playing", "upcoming"];
-    return (
-      <Tabs>
-                <TabList>
-                <div className="tab-panel-content">
-                  <Tab>
-                    {listofMovies.map((listofMovie) => (
-                    <button  key={listofMovie} onClick={() => fetchMovies(listofMovie)}>{listofMovie}</button>
-                  ))}
-                  </Tab>
-                    </div>
-                </TabList>
+  const listofMovies = ["popular", "top_rated", "now_playing", "upcoming"];
+  return (
+    <Tabs>
+      <TabList>
+        <div className="tab-panel-content">
+          <Tab>
+            {listofMovies.map((listofMovie) => {
+              const formattedName = listofMovie
+                .replace(/_/g, " ")
+                .toUpperCase();
+              return (
+                <button
+                  key={listofMovie}
+                  onClick={() => fetchMovies(listofMovie)}
+                >
+                  {formattedName}
+                </button>
+              );
+            })}
+          </Tab>
+        </div>
+      </TabList>
 
-                <TabPanel>
-                   
-                    
-                </TabPanel>
-      </Tabs>
+      <TabPanel></TabPanel>
+    </Tabs>
 
+    
+  );
+};
 
-      // <nav>
-      //   <ul>
-      //     {listofMovies.map((listofMovie) => (
-      //       <li key={listofMovie}>
-      //         <button onClick={() => fetchMovies(listofMovie)}>{listofMovie}</button>
-      //         </li>
-      //     ))}
-      //   </ul>
-  
-      // </nav>
-  
-    )
-}
-
-export default Categories
+export default Categories;
