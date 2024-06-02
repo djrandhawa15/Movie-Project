799211ad23f0e07b0d2b82b2ffa6ec11ba93d6e7
Priya Tyagi <priyatyagi1905@gmail.com>
2024-06-02 11:51:38 -0700

    fixed about page for all screen sizes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,15 @@ import '../styles/about.css';
 
 const About = () => {
   return (
-    <div className="about-container">
-      <div className="side-photo">
+        <section className="about-container">
+    <div className="side-photo">
         <img src={sidephoto} alt="Photo of Orpheum theatre in Vancouver" />
       </div>
-      <div className="text-and-logo-container">
-        <div className="text-content">
-          <h6>This product uses the TMDb API but is not endorsed or certified by TMDb.</h6>
+     
+       
+         <article className='about-info'>
+
+        
           <p>CINE HUB is an innovative movie database project designed to provide users with a comprehensive and immersive movie experience. CINE HUB offers a vast collection of information about movies, TV shows, actors, and more.</p>
           <br />
           <p>Created by: 
@@ -23,14 +25,15 @@ const About = () => {
               <li>Mudarres - <a href="http://xyz.com">Mudarres's Website</a></li>
               </ul>
               </p>
-        </div>
+       
+        <p>This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
         <div className="tmdb-container">
           <NavLink to='https://www.themoviedb.org/' target='_blank' rel='noopener noreferrer'>
-            <img className='tmdb' src={tmdbLogo} alt="TMDb Logo" />
+            <img  src={tmdbLogo} alt="TMDb Logo" />
           </NavLink>
         </div>
-      </div>
-    </div>
+        </article>
+    </section>
   )
 }
 
